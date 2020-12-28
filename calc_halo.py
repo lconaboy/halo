585d899dc9be504e72dd5a821398ad0e2bb59b82
Luke Conaboy <lukeconaboy@gmail.com>
2020-12-28 20:35:49 +0000

    Found loads of bugs

--- calc_halo.py
+++ calc_halo.py
@@ -1,7 +1,6 @@
+import seren3
 import numpy as np
 from seren3.array import SimArray
-from cosmolopy.distance import e_z
-from cosmolopy.density import cosmo_densities, omega_M_z
 
 def M_to_R(m, f, cosmo):
     """Converts from a halo mass defined as f wrt the critical density
@@ -20,6 +19,9 @@ def M_to_R(m, f, cosmo):
 
     :rtype: (float)
     """
+    from cosmolopy.distance import e_z
+    from cosmolopy.density import cosmo_densities, omega_M_z
+
 
     assert(f == 'vir' or (type(f) == type(1.) or
            type(f) == type(1))), "f should be 'vir' or number"
@@ -28,8 +30,12 @@ def M_to_R(m, f, cosmo):
     # H(z) = H0 * E(z)
     # rho_crit(z) = rho_crit(0) * E(z)**2
     
-    rho_crit, rho_bar = cosmo_densities(**cosmo)
-    E_z = e_z(cosmo['z'], **cosmo)
+    # FIXME this doesn't work in this instance for some reason, e_z
+    # throws an error
+    z = cosmo['z']
+    print z
+    rho_crit, _ = cosmo_densities(**cosmo)
+    E_z = e_z(z=z, **cosmo)
     rho_crit_z = rho_crit * E_z**2
     omega_m_z = omega_M_z(z, **cosmo)
     
@@ -38,13 +44,13 @@ def M_to_R(m, f, cosmo):
     else:
         X = float(f)
 
-    r = ((3 .* m)/(4. * np.pi * X * rho_crit_z)) ** 1./3.  # Mpc
+    r = ((3. * m)/(4. * np.pi * X * rho_crit_z)) ** 1./3.  # Mpc
     r = r * 1e3 * cosmo['h'] / cosmo['aexp']  # a kpc h**-1
     
     return r
 
         
-def main(path, ioutput)
+def main(path, ioutput):
     from seren3.analysis.parallel import mpi
     
     if mpi.host:
@@ -64,6 +70,7 @@ def main(path, ioutput)
         halo_ix = halos.halo_ix(shuffle=True)[0:3]
 
     dest = {}
+    # FIXME rewrite MPI loop
     for i, sto in mpi.piter(halo_ix, storage=dest, print_stats=False):
         h = halos[i]
 
@@ -82,15 +89,20 @@ def main(path, ioutput)
 
         # The quantities calculated at R200c, as determined by the
         # M200c returned by rockstar
-        M200c_rs = h.properties['m200c']/subsnap.cosmo['h']        # Msol
-        R200c_rs = M_to_R(M200c_rs, f=200,
-                          cosmo=subsnap.cosmo)/subsnap.cosmo['h']  # a * kpc
+
+        # FIXME bug in M_to_R
+        # M200c_rs = h.properties['m200c']/subsnap.cosmo['h']        # Msol
+        # R200c_rs = (M_to_R(M200c_rs, f=200,
+        #                    cosmo=subsnap.cosmo)/subsnap.cosmo['h'])  # a * kpc
 
 
         sto.idx = h["id"]
-        sto.result = {"M_tot" : M_tot, "R_vir" : R_vir.in_units('km'), "M_gas" : M_gas, \
-                          "M_star" : s_dset["mass"].in_units("Msol").sum()}
+        # sto.result = {"M_tot" : M_tot, "R_vir" : R_vir.in_units('km'), "M_gas" : M_gas, \
+        #                   "M_star" : s_dset["mass"].in_units("Msol").sum()}
 
+        sto.result = {'Mvir_rs': Mvir_rs, 'Rvir_rs': Rvir_rs,
+                      'Mvir_dm': Mvir_dm, 'Mvir_gas': Mvir_gas, 
+                      'Mvir_star': Mvir_star}
             
 
 
@@ -99,7 +111,7 @@ def main(path, ioutput)
         
         fn = os.path.join(path, 'out_{0:d}.h5'.format(ioutput))
         res = mpi.unpack(dest)
-        with h5py.File(, 'w') as f:
+        with h5py.File(fn, 'w') as f:
             g = f.create_group('Results')
             for key in res:
                 g.create_dataset(key, res[key])
